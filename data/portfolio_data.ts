0620a23d600a57388d8d3244791f2dda07322642
Esteban Cortes R <hola@ecortes.cl>
2025-03-18 16:03:23 -0300

    :bento: add news portfolio

--- data/portfolio_data.ts
+++ data/portfolio_data.ts
@@ -4,7 +4,12 @@ import portfolio_img_2 from "~/assets/images/projects/work2.webp";
 import portfolio_img_3 from "~/assets/images/projects/work3.webp";
 import portfolio_img_4 from "~/assets/images/projects/work4.webp";
 import portfolio_img_5 from "~/assets/images/projects/work5.webp";
- 
+import portfolio_img_6 from "~/assets/images/projects/work6.webp";
+import portfolio_img_7 from "~/assets/images/projects/work7.webp";  
+import portfolio_img_8 from "~/assets/images/projects/work8.webp";  
+import portfolio_img_9 from "~/assets/images/projects/work9.webp";  
+
+
 const portfolio_data = [
   {
     id: 1,
@@ -50,38 +55,38 @@ const portfolio_data = [
   {
     id: 6,
     col: 3,
-    img: portfolio_img_5,
-    category: "Nuxt 3 + Tailwind",
-    title: "almachamisero.cl",
-    description: "Desarrollado para la agencia Quiebre.cl",
+    img: portfolio_img_6,
+    category: "Laravel + Vue",
+    title: "xrsgym.fit",
+    description: "Desarrollado con resolutionsweb.cl",
   }
   ,
   {
     id: 7,
     col: 3,
-    img: portfolio_img_5,
-    category: "Nuxt 3 + Tailwind",
-    title: "almachamisero.cl",
-    description: "Desarrollado para la agencia Quiebre.cl",
+    img: portfolio_img_7,
+    category: "Wordpress + Elementor",
+    title: "xrsgym.fit",
+    description: "Desarrollado con resolutionsweb.cl",
   } 
   
   ,
   {
     id: 8,
     col: 3,
-    img: portfolio_img_5,
-    category: "Nuxt 3 + Tailwind",
-    title: "almachamisero.cl",
-    description: "Desarrollado para la agencia Quiebre.cl",
+    img: portfolio_img_8,
+    category: "ReactNative",
+    title: "XRSGYM IOS/Android",
+    description: "Desarrollado con resolutionsweb.cl",
   } 
   ,
   {
     id: 9,
     col: 3,
-    img: portfolio_img_5,
-    category: "Nuxt 3 + Tailwind",
-    title: "almachamisero.cl",
-    description: "Desarrollado para la agencia Quiebre.cl",
+    img: portfolio_img_9,
+    category: "Laravel Api + Swagger",
+    title: "xrsgym.fit",
+    description: "Desarrollado con resolutionsweb.cl",
   } 
 ];
 
